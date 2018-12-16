6e7bfe956fff0c75f43810a2d2ac0ac6fd771461
Alan Jones <jones.alan21@gmail.com>
2018-12-16 15:30:46 -0500

    fixing URI ref

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const server = require('./api/server');
 const mongoose = require('mongoose')
 
-const { PORT, DB_URI } = require('./api/utils/constants');
+const { PORT, MONGODB_URI } = require('./api/utils/constants');
 
 mongoose.connect(MONGODB_URI)
 
 server.listen(PORT, async () => {
-    await mongoose.connect(DB_URI)
+    await mongoose.connect(MONGODB_URI)
     console.log(`App listening on port ${PORT}`)
 });
